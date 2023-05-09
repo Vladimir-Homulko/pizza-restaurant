d657ea5586f4cb6b09a73c79fe823296029ca6f8
Vladimir Homulko <homulko.job@gmail.com>
2023-05-09 17:38:24 +0300

    Fix: inject restaurant service in constructor

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import express, { Express } from "express";
 import bodyParser from "body-parser";
 import {RestaurantController} from "./src/controllers/restaurant.controller";
 import * as mongoose from "mongoose";
+import {RestaurantService} from "./src/services/restaurant.service";
 
 dotenv.config();
 
@@ -12,15 +13,13 @@ const app: Express = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const restaurantController = new RestaurantController();
+const restaurantController = new RestaurantController(new RestaurantService());
 
 app.post('/', restaurantController.createAndProcessOrders);
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string, {
-      // authSource: 'admin',
-    });
+    await mongoose.connect(process.env.MONGO_URI as string);
     app.listen(PORT, () => console.log(`Started on ${PORT} port...`));
   } catch (error) {
     console.log(error);
--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -4,11 +4,14 @@ import {RestaurantService} from "../services/restaurant.service";
 
 export class RestaurantController {
 
+  constructor(private restaurantService: RestaurantService) {
+    this.restaurantService = restaurantService;
+  }
+
   public async createAndProcessOrders(req: Request, res: Response) {
     try {
     const orders: Order[] = req.body;
-    const restaurantService = new RestaurantService();
-    await restaurantService.processOrders(orders);
+    await this.restaurantService.processOrders(orders);
     res.sendStatus(200);
     } catch (err) {
       res.status(500).send(err);
